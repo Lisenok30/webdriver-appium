74bb009a79091f053881c03b86025f2f43b32142
Lisenok30 <lidia.utesheva@gmail.com>
2022-10-18 12:26:37 -0700

    new update

--- test/specs/android/delete-note.screen.spec.js
+++ test/specs/android/delete-note.screen.spec.js
@@ -1,41 +1,38 @@
 const EditNoteScreen = require("../../screenobjects/android/edit-note.screen");
 
-describe('Delete Note', () => {
+describe("Delete Note", () => {
+  before(async () => {
+    await EditNoteScreen.skipTutorial();
+    await EditNoteScreen.addNoteAndSaveNote(
+      "TV Shows",
+      "Friends\nBreakingBad\nPeakyBlinders"
+    );
+    await driver.back();
+  });
 
-    before(async () => {
+  it("Delete a note & check the note in trash can", async () => {
+    const note = await EditNoteScreen.firstNote.getText();
 
-        await EditNoteScreen.skipTutorial();
-        await EditNoteScreen.addNoteAndSaveNote("TV Shows", "Friends\nBreakingBad\nPeakyBlinders");
-        await driver.back();
+    //click on the note
+    await EditNoteScreen.firstNote.click();
 
-    });
+    // click on more icon
+    await EditNoteScreen.moreIcon.click();
 
-    it('Delete a note & check the note in trash can', async () => {
-        
-        const note = await EditNoteScreen.firstNote.getText();
+    // click on delete item
+    await EditNoteScreen.deleteOption.click();
 
-        //click on the note
-         await EditNoteScreen.firstNote.click();
+    // accept alert
+    await driver.acceptAlert();
 
-         // click on more icon
-         await EditNoteScreen.moreIcon.click();
+    // click on nav icon
+    await EditNoteScreen.navIcon.click();
 
-         // click on delete item
-         await EditNoteScreen.deleteOption.click();
+    // click on trash can item
+    await EditNoteScreen.trashCan.click();
 
-         // accept alert
-         await driver.acceptAlert();
-
-         // click on nav icon
-         await EditNoteScreen.navIcon.click();
-
-         // click on trash can item
-         await EditNoteScreen.trashCan.click();
-
-         // assertion
-         const trashCanItem = await EditNoteScreen.firstNote;
-         await expect(trashCanItem).toHaveText(note);
-
-    });
-    
+    // assertion
+    const trashCanItem = await EditNoteScreen.firstNote;
+    await expect(trashCanItem).toHaveText(note);
+  });
 });
